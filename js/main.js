3c80c4985c01c8cc8dc32358712b2e5320e27ac6
Samuel <sjoseph1848@gmail.com>
2019-04-18 12:58:27 -0500

    updated comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+//Sticks the navbar at the top when we scroll, reduces opacity
 window.addEventListener('scroll', function() {
   if (window.scrollY > 150) {
     document.querySelector('#navbar').style.opacity = 0.9;
@@ -8,6 +8,7 @@ window.addEventListener('scroll', function() {
 });
 
 
+//Found a way to smooth scroll from codepen 
 $('#navbar a, .btn').on('click', function(event) {
   if (this.hash !== '') {
     event.preventDefault();
